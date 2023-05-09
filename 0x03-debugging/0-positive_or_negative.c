82bf7a5494a8ef826164b03f3c4a850ec31b7b0f
Michkiri <miriokiri@gmail.com>
2023-05-08 18:01:11 -0700

    Multiple mains

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry of a program
+ * Return: Always return (0)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+	{
+		if (n > 0)
+		{
+			printf("%i is positive\n", n);
+		}
+		else
+		{
+			printf("%i is zero\n", n);
+		}
+	}
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+My first debugging took me a while but i finally got it by doing it. it had only four tasks, 0-4

a2e840fa9e93bc95001416e3f07ee578de9c8f34
Michkiri <miriokiri@gmail.com>
2023-05-31 09:22:52 -0700

    referring

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,9 +7,9 @@
  */
 int main(void)
 {
-    int n;
+    char *str;
 
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
     return (0);
 }
